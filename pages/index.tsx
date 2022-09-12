bffb9df0e65410ff72cb16227a07f924f895f39d
Jobin Biju <jobinbiju9090@gmail.com>
2022-09-12 23:39:26 +0530

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ type Image = {
   href: string
   imageSrc: string
   name: string
-  username: string
+  userName: string
 }
 
 export default function Gallery({ images }: { images: Image[] }) {
@@ -45,14 +45,14 @@ function BlurImage({ image }: { image: Image }) {
 
   return (
     <a href={image.href} className="group">
-      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-black xl:aspect-w-7 xl:aspect-h-8">
         <Image
           alt=""
           src={image.imageSrc}
           layout="fill"
           objectFit="cover"
           className={cn(
-            'duration-700 ease-in-out group-hover:opacity-75',
+            'duration-500 ease-in-out group-hover:opacity-70',
             isLoading
               ? 'scale-110 blur-2xl grayscale'
               : 'scale-100 blur-0 grayscale-0'
@@ -61,7 +61,7 @@ function BlurImage({ image }: { image: Image }) {
         />
       </div>
       <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
+      <p className="mt-1 text-lg font-medium text-gray-900">{image.userName}</p>
     </a>
   )
 }
